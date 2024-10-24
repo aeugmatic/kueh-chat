60e0210548062313fb6abfc4c4c0a60fe6c5f24f
aeugmatic <89606750+aeugmatic@users.noreply.github.com>
2024-10-24 23:01:14 +0100

    Update kueh.js
    TODO addition

--- source-files/kueh.js
+++ source-files/kueh.js
@@ -5,6 +5,7 @@
     - Use a better parallax algorithm / formula
     - Consider using a more "consistent" method for adding images to text (since badge and emote adding functions do it differently)
     - Flesh out the `escapeText` function / XSS prevention further
+    - Consider potentially deleting message divs once they disappear off-screen, if allowing them to remain affects performance
 */
 
 window.addEventListener("onEventReceived", (obj) => {
